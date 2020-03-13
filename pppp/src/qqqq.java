35fcf271d7baa90f610df34a4ecf066f5e207fa5
zhangmk <1585626256@qq.com>
2020-03-13 11:24:36 +0800

    Initial commit

---
+++ pppp/src/qqqq.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class qqqq {
+    public static final String a="kkkk";
+   public static final int q=9;
+   public static final int qoo=98;
+    private ArrayList list;
+
+    public static String getA() {
+        return a;
+    }
+
+    public static int getQ() {
+        return q;
+    }
+
+    public static int getQoo() {
+        return qoo;
+    }
+public int fang(){
+    list = new ArrayList();
+
+    list.add(1111);
+    list.add(9999);
+
+    return 0;
+}
+    public static void main(String[] args) {
+        System.out.println("a = " + a);
+    }
+}
